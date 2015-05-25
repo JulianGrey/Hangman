4ec6cdbe937186df47b9ba75c770a1f8f3af64ce
Julian <julianrgrey@gmail.com>
2015-05-25 22:31:39 +0100

    Implement logic for choosing a random word from external text file

--- hangman.py
+++ hangman.py
@@ -1,2 +1,14 @@
+from random import randint
+
+
 def hangman_game():
-    pass
+    chosen_word = random_word()
+    print chosen_word
+
+
+def random_word():
+    f = open('words.txt', 'r')
+    words = f.read().split('\n')
+    f.close()
+
+    return words[randint(0, len(words) - 1)]
